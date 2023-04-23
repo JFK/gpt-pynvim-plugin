7c1bbafac6af56f023178e7aa1e8031760539d18
Fumikazu Kiyota <kiyota@fumikazunombp.lan>
2023-04-23 17:26:08 +0900

    Update tests and yml

--- tests/test_gpt_pynvim.py
+++ tests/test_gpt_pynvim.py
@@ -1,4 +1,5 @@
 import os
 
 def test_openai_api_key():
-    assert 'OPENAI_API_KEY' in os.environ, "OPENAI_API_KEY environment variable not set"
+    assert 'OPENAI_API_KEY' in os.environ, \
+        "OPENAI_API_KEY environment variable not set"
